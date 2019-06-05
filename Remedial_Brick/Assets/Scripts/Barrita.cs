abce5bec4d87d8dc74a7fb3cb94d5b934ecc1e2a
Cristina B <bau161260@uvg.edu.gt>
2019-06-04 21:54:33 -0600

    Ya se mueve a los lados la barrita

---
+++ Remedial_Brick/Assets/Scripts/Barrita.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrita : MonoBehaviour
+{
+    float velocidad = 10f;
+    float noPasarDerecha = 5.07f;
+    float noPasarIzquierda = -5.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * velocidad);
+
+        if(transform.position.x > noPasarDerecha)
+        {
+            transform.position = new Vector2(noPasarDerecha, transform.position.y);
+        }
+
+        if (transform.position.x < noPasarIzquierda)
+        {
+            transform.position = new Vector2(noPasarIzquierda, transform.position.y);
+        }
+    }
+}
---
+++ Remedial_Brick/Assets/Scripts/Pelotita.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pelotita : MonoBehaviour
+{
+    public float velocidad = 10f;
+    public Rigidbody2D rb;
+    bool encima;
+    public Transform barra;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.up * 250);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!encima)
+        {
+
+        }
+    }
+
+
+    public void Reinicio()
+    {
+        transform.position = new Vector3(-0.07f, -3.52f, 0.90625f);
+        GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle.normalized * velocidad;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Piso"))
+        {
+
+        }
+    }
+}
